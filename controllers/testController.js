db557f7582208177ce11df0488bdb734792d9801
Ckerarahpikc <werrrom@gmail.com>
2024-08-27 16:16:46 +0300

    Updated .gitignore and removed unnecessary files from tracking

--- controllers/testController.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const User = require("../models/testModel");
-const catchAsync = require("../utils/catchAsync");
-
-// * insert
-exports.goInsert = catchAsync(async (req, res, next) => {});
