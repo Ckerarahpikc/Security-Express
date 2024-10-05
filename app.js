18fec42e49fa1bf0d26278929a7f31be38920a06
Ckerarahpikc <werrrom@gmail.com>
2024-10-05 13:39:33 +0300

    adding 'getNeighbor' which basically will return the restaurants that the person location are close to

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const RateLimit = require("express-rate-limit");
 const mongoSanitize = require("express-mongo-sanitize");
 
 const GEC = require("./controllers/globalErrorController");
-const testRoute = require("./routes/testRoute");
+const viewRouter = require("./routes/viewRouter");
 const securityMiddleware = require("./securityTools/helmetSecure");
 const sanitizeAThingOrTwo = require("./securityTools/xssFilter");
 
@@ -32,45 +32,45 @@ app.use(hpp());
 app.use(securityMiddleware);
 
 // 3. rateLimit - this function accepts an options object and returns the rate limiting middleware (notice: there is not required configurations, all options have reasonable defaults)
-const limiter = RateLimit({
-  limit: 100, // the maximum allowed request that can user make from a single IP address ('max' serves the same purpose as 'limit')
-  windowMs: 60 * 60 * 1000, // the rate limit (which is set to 1hour), meaning that user can make up to 100 request within this period
-  standardHeaders: true, // this ensures that the rate information (like remaining request) is included in the 'RateLimit-*' headers in the HTTP response (this info is useful for the clients that makes request and other things when working with let's say API)
-  legacyHeaders: false, // this disables the older X-RateLimit-* headers, which helps to avoid redundancy and keeps the response header cleaner
-  message: "You've reached the rate limit. Try again after 1h.", // as u may already understand, this is a custom message, which basically tell the client that they've reached the rate limit
+// const limiter = RateLimit({
+//   limit: 100, // the maximum allowed request that can user make from a single IP address ('max' serves the same purpose as 'limit')
+//   windowMs: 60 * 60 * 1000, // the rate limit (which is set to 1hour), meaning that user can make up to 100 request within this period
+//   standardHeaders: true, // this ensures that the rate information (like remaining request) is included in the 'RateLimit-*' headers in the HTTP response (this info is useful for the clients that makes request and other things when working with let's say API)
+//   legacyHeaders: false, // this disables the older X-RateLimit-* headers, which helps to avoid redundancy and keeps the response header cleaner
+//   message: "You've reached the rate limit. Try again after 1h.", // as u may already understand, this is a custom message, which basically tell the client that they've reached the rate limit
 
-  // there is also other USEFUL options, like:
-  // when u want to rate limit based on something other than the default IP adress (e.g. user ID, ...)
-  keyGenerator: (req) => req.ip, // default
+//   // there is also other USEFUL options, like:
+//   // when u want to rate limit based on something other than the default IP adress (e.g. user ID, ...)
+//   keyGenerator: (req) => req.ip, // default
 
-  // use this when U want to send more detailed messages or logging rate limit hits
-  handler: (req, res) => {
-    res
-      .status(429)
-      .json({ message: "Your gentleman message for the clients." });
-  },
+//   // use this when U want to send more detailed messages or logging rate limit hits
+//   handler: (req, res) => {
+//     res
+//       .status(429)
+//       .json({ message: "Your gentleman message for the clients." });
+//   },
 
-  // if u want to exclude certain routes, users, or IPs from rate limiting (e.g., internal services or admin users) use this
-  skip: (req) => req.user && req.user.role === "admin",
-  // example with IP (assume we have allowlist = ['192.168.0.56', '192.168.0.21'])
-  skip: (req, res) => allowlist.includes(req.ip)
+//   // if u want to exclude certain routes, users, or IPs from rate limiting (e.g., internal services or admin users) use this
+//   skip: (req) => req.user && req.user.role === "admin",
+//   // example with IP (assume we have allowlist = ['192.168.0.56', '192.168.0.21'])
+//   skip: (req, res) => allowlist.includes(req.ip)
 
-  // useful for distributed environments where you want to share limits across multiple servers
-  // store: new RedisStore({
-  //   client: redisClient
-  // }),
+//   // useful for distributed environments where you want to share limits across multiple servers
+//   // store: new RedisStore({
+//   //   client: redisClient
+//   // }),
 
-  // this can be used for monitoring or to trigger/alert/logging or other actions
-  // onLimitReached: (req, res, options) => {
-  //   console.log(`Rate limit reached for IP: ${req.ip}`);
-  // } // ! option is deprecated and has been removed in express-rate-limit v7, but don't worry nobody use it anyway
-});
+//   // this can be used for monitoring or to trigger/alert/logging or other actions
+//   // onLimitReached: (req, res, options) => {
+//   //   console.log(`Rate limit reached for IP: ${req.ip}`);
+//   // } // ! option is deprecated and has been removed in express-rate-limit v7, but don't worry nobody use it anyway
+// });
 // but personally I'm gonna use this
 const limiterMongo = RateLimit({
   store: new MongoStore({
     uri: process.env.MONGO_DRIVER.replace(
-      "<password>",
-      process.env.MONGO_PASSWORD
+      "<db_password>",
+      process.env.MONGO_DRIVER_PASSWORD
     ),
     // should match windowMs
     expireTimeMs: 30 * 60 * 1000,
@@ -81,7 +81,7 @@ const limiterMongo = RateLimit({
   windowMs: 30 * 60 * 1000 // 30 min
 });
 // then let's say that I have this route (host/websiteName/users/plans) and I want to use limit rate, I'll do it like this:
-app.use("/api", limiterMongo);
+app.use("/", limiterMongo);
 // U can use this for all of your routes
 // app.use(limiterMongo);
 
@@ -123,11 +123,9 @@ const payload = {
 app.use(sanitizeAThingOrTwo);
 
 // routes ===================================================
-app.route("/").get((req, res) => {
-  res.status(200).render("layout");
-});
-// app.use("/api/users", testRoute);
+app.use("/", viewRouter);
 
+// global error handler
 app.use(GEC);
 
 module.exports = app;
